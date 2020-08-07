ffca39b89a22291142b6ec05da51c6ddc6c0c701
Jess Daniel <jessdaniel474@gmail.com>
2020-08-07 13:45:28 -0400

    ex2, ex3, ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,19 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
+    for num in a:
+        if num not in cache:
+            cache[num] = None
+        if (num * -1) in cache:
+            cache[num*-1] = num
+            cache[num] = num * -1
+
+    result = []
+    for num in cache:
+        if num > 0 and cache[num] is not None:
+            result.append(num)
     return result
 
 
